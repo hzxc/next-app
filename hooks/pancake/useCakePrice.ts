5b2878363a9bc5a4b8cb95a78d1a5109e3e73f77
Your Name <you@example.com>
2022-10-18 02:09:18 +0800

    2022-10-18 02:09

--- hooks/pancake/useCakePrice.ts
+++ hooks/pancake/useCakePrice.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from 'react-query';
 import { PancakeRouterABI } from 'abi/bsc';
 import { ethers, utils } from 'ethers';
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,9 @@ import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-// import { WagmiConfig } from 'wagmi';
 import { wagmiClient } from 'conf';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
