472b5c6b4a25d29d1cb335437ae81d838733f1b7
Your Name <you@example.com>
2022-10-20 19:47:48 +0800

    2022-10-20 19:47

--- pages/samples/ethers/multicall.tsx
+++ pages/samples/ethers/multicall.tsx
@@ -7,17 +7,19 @@ import { ReactElement } from 'react';
 
 const Multicall: NextPageWithLayout = () => {
   const iface = new ethers.utils.Interface(IBEP20ABI);
-  const abiCoder = ethers.utils.defaultAbiCoder;
+  // const abiCoder = ethers.utils.defaultAbiCoder;
   const hash = iface.getSighash('balanceOf');
-  const abiC = abiCoder.encode(['uint', 'string'], [1234, 'Hello World']);
-  const encodeFunc = iface.encodeFunctionData('aggregate', [
-    { target: '', callData: '' },
-    { target: '', callData: '' },
-  ]);
+  // const abiC = abiCoder.encode(['uint', 'string'], [1234, 'Hello World']);
+  // const encodeFunc = iface.encodeFunctionData('aggregate', [
+  //   { target: '', callData: '' },
+  //   { target: '', callData: '' },
+  // ]);
+  // 0xe2e4263a
+  // 0xe2e4263a
   return (
     <div className='p-8'>
       <p>hash:{hash}</p>
-      <p>encodeFunc:{abiC}</p>
+      {/* <p>encodeFunc:{abiC}</p> */}
     </div>
   );
 };
