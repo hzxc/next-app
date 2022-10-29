dd7abbb7702c198710b381ea645aeb915cd5bc5b
Your Name <you@example.com>
2022-10-30 01:16:55 +0800

    2022-10-30 01:16

--- pages/samples/ethers/uniswap-fetcher.tsx
+++ pages/samples/ethers/uniswap-fetcher.tsx
@@ -3,14 +3,28 @@ import { NextPageWithLayout } from 'pages/_app';
 import { ReactElement, useEffect } from 'react';
 import { Fetcher, Trade, Token } from '@uniswap/sdk';
 import { bscBusdAddr, bscCakeAddr } from 'data';
+import { Button } from 'components';
+import { bscProvider } from 'conf';
 
 const UniswapFetcher: NextPageWithLayout = () => {
-  const CAKE = new Token(56, bscCakeAddr, 18);
-  const BUSD = new Token(56, bscBusdAddr, 18);
+  // const CAKE = new Token(56, bscCakeAddr, 18);
+  // const BUSD = new Token(56, bscBusdAddr, 18);
 
-  useEffect(() => {}, []);
+  const handleFetchPair = async () => {
+    const CAKE = await Fetcher.fetchTokenData(56, bscCakeAddr, bscProvider);
+    const BUSD = await Fetcher.fetchTokenData(56, bscBusdAddr, bscProvider);
+    console.log(CAKE);
+    console.log(BUSD);
+    const pairs = await Fetcher.fetchPairData(CAKE, BUSD, bscProvider);
+    console.log(pairs);
+  };
 
-  return <div className='p-8'>BestTrade</div>;
+  return (
+    <div className='p-8'>
+      <Button onClick={handleFetchPair}>fetch pair data</Button>
+      {/* <p>{JSON.stringify('')}</p> */}
+    </div>
+  );
 };
 
 UniswapFetcher.getLayout = function getLayout(page: ReactElement) {
