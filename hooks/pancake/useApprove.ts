b17b643d8024529901b05798dd2c9b84cc96cbde
Your Name <you@example.com>
2023-01-11 00:56:52 +0800

    2023-1-11 00:56

---
+++ hooks/pancake/useApprove.ts
@@ -0,0 +1,24 @@
+import { ChainId } from 'eth';
+import { useEffect, useState } from 'react';
+import { useNetwork } from 'wagmi';
+
+export const useApprove = () => {
+  const { chain } = useNetwork();
+
+  const [chainId, setChainId] = useState(ChainId.BSC);
+
+  useEffect(() => {
+    let id = chain?.id ?? ChainId.BSC;
+    if (
+      id !== ChainId.BSC &&
+      id !== ChainId.ETHEREUM &&
+      id !== ChainId.BSC_TESTNET
+    ) {
+      id = ChainId.BSC;
+    }
+
+    setChainId(id);
+  }, [chain]);
+
+  return [chainId] as const;
+};
